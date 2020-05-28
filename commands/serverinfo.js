288766568558eb6c688984a75d7b8e653a551c1a
Kevlaris <64031412+Kevlaris@users.noreply.github.com>
2020-05-28 19:38:58 +0200

    New command

---
+++ commands/serverinfo.js
@@ -0,0 +1,9 @@
+modules.exports = {
+	name: 'serverinfo',
+	description: 'Shows server info.',
+	aliases: ['serverinf', 'guildinfo', 'guild'],
+	execute (message) {
+		if (message.channel.type === 'dm') return message.channel.send('You need to be in a server to use this command.')
+		message.channel.send(`**__Server Information__**\n**Server Name:** &{message.channel.guild.name}\n**Server Owner:** &{message.channel.guild.owner.user.username}\n**Number of Members:** &{message.channel.guild.memberCount}`)
+	},
+};
