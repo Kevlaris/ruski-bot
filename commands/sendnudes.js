64892d0164d36731a968b6da57538307e7b5e5fa
Kevlaris <64031412+Kevlaris@users.noreply.github.com>
2020-05-29 18:44:10 +0200

    shh

---
+++ commands/sendnudes.js
@@ -0,0 +1,35 @@
+module.exports = {
+	async execute(message, args) {
+		const defaultNude = 'Someone just wanted to send you a nude.. watch out for your friends! ;3';
+
+		const mentioned = message.mentions.users.first();
+		if(!mentioned) return message.reply('you need to mention someone! ;3');
+
+		var member;
+		try {
+			member = await message.channel.guild.members.fetch(mentioned);
+		}
+		catch (error) {
+			member = null;
+			console.log(error);
+		}
+		if(!member) return message.reply('I couldn\'t find the specified member. 3:');
+
+		const nudes = args.splice(1).join(' ');
+		if(!nudes) {
+			try {
+				return mentioned.send(defaultNude);
+			} catch (warning) {
+				message.reply('I couldn\'t reach the specified member.');
+				return console.warn(warning);
+			}
+		}
+
+		try {
+			mentioned.send(nudes);
+		}
+		catch (err) {
+			return console.warn(err);
+		}
+	},
+};
