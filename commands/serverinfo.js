afde2462f446214a996aded8a5ff9082aa61d67b
Kevlaris <64031412+Kevlaris@users.noreply.github.com>
2020-05-29 13:34:30 +0200

    1.1.2
    Updated serverinfo command

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,9 +1,26 @@
+const Discord = require('discord.js');
+const { client } = require('../index.js');
+
 module.exports = {
 	name: 'serverinfo',
 	description: 'Shows server info.',
 	aliases: ['serverinf', 'guildinfo', 'guild'],
 	execute(message) {
 		if (message.channel.type === 'dm') return message.channel.send('You need to be in a server to use this command.');
-		message.channel.send('**__Server Information__**\n**Server Name:** ' + message.channel.guild.name + '\n**Server Owner:** ' + message.channel.guild.owner.user.username + '\n**Number of Members:** ' + message.channel.guild.memberCount);
+
+		const guild = message.channel.guild;
+
+		const embed = new Discord.MessageEmbed()
+			.setAuthor(client.user.tag, client.user.avatarURL)
+			.setTitle(guild.name)
+			.setDescription('Server Information')
+			.addFields(
+				{ name: 'Owner', value: guild.owner.user.tag, inline: true },
+				{ name: 'Number of Members', value: guild.memberCount, inline:true },
+				{ name: 'Region', value: guild.region, inline: true },
+			)
+			.setFooter('Server Information');
+		message.channel.send(embed);
+		console.log(guild.icon);
 	},
 };
