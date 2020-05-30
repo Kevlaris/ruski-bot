d27f574f2363654289552dd4f2545f5a613343cf
Kevlaris <64031412+Kevlaris@users.noreply.github.com>
2020-05-30 08:18:52 +0200

    Update setlogchannel.js

--- commands/setlogchannel.js
+++ commands/setlogchannel.js
@@ -3,11 +3,17 @@ module.exports = {
 	description: 'Sets the log channel in the server.',
 	usage: '[channel]',
 	execute(message, args) {
-		if(!message.member.hasPermission('MANAGE_SERVER' || 'ADMINISTRATOR') {return message.reply('you don\'t have the permissions to change the log channel.');}
+		if(!message.member.hasPermission('MANAGE_SERVER' || 'ADMINISTRATOR') {
+		   return message.reply('you don\'t have the permissions to change the log channel.');
+		}
 	
 		const logChannel = args[0];
-		if(!logChannel) {return message.reply('you need to specify a channel.');}
-		if(logChannel.type === !'Channel') {return message.reply('you need to specify a channel.');}
+		if(!logChannel) {
+			return message.reply('you need to specify a channel.');
+		}
+		if(logChannel.type === !'Channel') {
+			return message.reply('you need to specify a channel.');
+		}
 	
 		try {
 			message.client.logChannels.set(message.channel.guild.id, logChannel);
